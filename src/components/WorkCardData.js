915d6a886ea6b2cc4450f2e8c1ae7ef1f35c746a
ClaireFree <claire@robingoodfoods.com>
2023-02-09 16:50:24 -0800

    added Project 5

--- src/components/WorkCardData.js
+++ src/components/WorkCardData.js
@@ -2,6 +2,7 @@ import pro1 from "../assets/img/Project1.png";
 import pro2 from "../assets/img/Project2.png";
 import pro3 from "../assets/img/Project3.png";
 import pro4 from "../assets/img/Project4.png";
+import pro5 from "../assets/img/Project5.gif";
 
 const ProjectCardData = [
     {
@@ -37,11 +38,11 @@ const ProjectCardData = [
     },
 
     {
-        imgsrc: pro3,
-        title: "RobinGoodFoods",
+        imgsrc: pro5,
+        title: "Note-Taker",
         text: "",
-        website: "https://claire-max.github.io/urban-sniffle/",
-        github:"https://github.com/claire-max/RobinGoodFoods",
+        website: "https://note-app123553.herokuapp.com/",
+        github:"https://github.com/claire-max/Note-Taker",
     },
 
     {
