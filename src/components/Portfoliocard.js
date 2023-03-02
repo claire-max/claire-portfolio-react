ac612f393de4b0b68884d690b83b76b9d6b9beb2
ClaireFree <claire@robingoodfoods.com>
2023-03-02 15:03:18 -0800

    fixed bug

--- src/components/Portfoliocard.js
+++ src/components/Portfoliocard.js
@@ -5,7 +5,7 @@ import React from 'react'
 const Portfoliocard = (props) => {
   return (
     <div className="project-card">
-        <img src={props.imgsrc} alt="image" /> 
+        <img className="projectimg" src={props.imgsrc} alt="image" /> 
         <h2 className="project-title">{props.title}</h2>
         <div className="pro-details">
             <p>{props.text}</p>
@@ -14,6 +14,7 @@ const Portfoliocard = (props) => {
             className="btn">Website</a>
             <a href= {props.github}
             className="btn">GitHub</a>
+            
         </div> 
 </div>
   );
